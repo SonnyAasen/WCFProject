c95e39a262b75e21e777249ef4c894cb92828426
SonnyAasen <35238145+SonnyAasen@users.noreply.github.com>
2018-12-11 02:35:11 +0100

    Rest Api implemented

---
+++ WCFProject/CarRentalWCFService/IRestService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CarRentalWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestService" in both code and config file together.
+    [ServiceContract]
+    public interface IRestService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            UriTemplate = "GetAvailableCarsByDates",
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Json,          
+            ResponseFormat = WebMessageFormat.Json)]
+        List<int> GetAvailableCarsByDates(DateTime fromDate, DateTime toDate);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "GetAllCars",
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        List<Car> GetAllCars();
+    }
+}
